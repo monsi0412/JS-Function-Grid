ecdd8c7249d40af9a0e26ab9c535564253f31d0d
Monserrat <monserratdiaz12@gmail.com>
2017-02-27 13:21:39 +0000

    did the 4 tasks

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 //Write your function calls below this line
+
+
+setColor(0, 0, "blue");
+setColor(9, 0, "blue");
+setColor(0, 9, "blue");
+setColor(9, 9, "blue");
+
+setColor(1, 1, "pink");
+setColor(1, 3, "pink");
+setColor(3, 0, "pink");
+setColor(4, 1, "pink");
+setColor(4, 2, "pink");
+setColor(4, 3, "pink");
+setColor(3, 4, "pink");
+
+setColor(5, 1, "purple");
+setColor(5, 2, "purple");
+setColor(5, 3, "purple");
+setColor(6, 1, "purple");
+setColor(7, 1, "purple");
+setColor(7, 2, "purple");
+setColor(7, 3, "purple");
+setColor(8, 3, "purple");
+setColor(9, 1, "purple");
+setColor(9, 2, "purple");
+setColor(9, 3, "purple");
+
+setColor(4, 4, "green");
+setColor(5, 4, "green");
+setColor(6, 4, "green");
+setColor(4, 5, "green");
+setColor(5, 5, "green");
+setColor(6, 5, "green");
+setColor(4, 8, "red");
+setColor(5, 8, "red");
+setColor(6, 8, "red");
+setColor(4, 9, "red");
+setColor(5, 9, "red");
+setColor(6, 9, "red");
